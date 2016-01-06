7ee3d7f933a90565fc6cee67190388e16f30c6cf
Subhag Oak <soak@microsoft.com>
2016-01-05 19:30:38 -0800

    CodePush (tested for android)
    I have added CodePush, formatted the code, renamed the customer to
    feedback.

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', "firebase", 'ui.bootstrap', 'ngCordova'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', "firebase", 'ui.bootstrap'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -16,11 +16,63 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', "
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
+    
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
-  });
+    
+    /* Invoke sync with the custom options, which enables user interaction.
+       For customizing the sync behavior, see SyncOptions in the CodePush documentation. 
+    */
+    window.codePush.sync(
+        function (syncStatus) {
+            switch (syncStatus) {
+                // Result (final) statuses
+                case SyncStatus.UPDATE_INSTALLED:
+                    displayMessage("The update was installed successfully. The changes will be visible after application restart. ");
+                    break;
+                case SyncStatus.UP_TO_DATE:
+                    displayMessage("The application is up to date.");
+                    break;
+                case SyncStatus.UPDATE_IGNORED:
+                    displayMessage("The user decided not to install the optional update.");
+                    break;
+                case SyncStatus.ERROR:
+                    displayMessage("An error occured while checking for updates");
+                    break;
+                
+                // Intermediate (non final) statuses
+                case SyncStatus.CHECKING_FOR_UPDATE:
+                    console.log("Checking for update.");
+                    break;
+                case SyncStatus.AWAITING_USER_ACTION:
+                    console.log("Alerting user.");
+                    break;
+                case SyncStatus.DOWNLOADING_PACKAGE:
+                    console.log("Downloading package.");
+                    break;
+                case SyncStatus.INSTALLING_UPDATE:
+                    console.log("Installing update");
+                    break;
+            }
+        },
+        {
+            installMode: InstallMode.ON_NEXT_RESTART, updateDialog: true
+        },
+        function (downloadProgress) {
+            console.log("Downloading " + downloadProgress.receivedBytes + " of " + downloadProgress.totalBytes + " bytes.");
+        }); 
+        
+        // Displays an alert dialog containing a message.
+        var displayMessage = function (message) {
+        navigator.notification.alert(
+            message,
+            null,
+            'CodePush',
+            'OK');
+        }   
+  });   
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
@@ -39,12 +91,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', "
     controller: 'AppCtrl'
   })
 
-  .state('tab.customer', {
-      url: '/customer',
+  .state('tab.feedback', {
+      url: '/feedback',
       views: {
-        'tab-customer': {
-          templateUrl: 'templates/tab-customer.html',
-          controller: 'CustomerCtrl'
+        'tab-feedback': {
+          templateUrl: 'templates/tab-feedback.html',
+          controller: 'FeedbackCtrl'
         }
       }
     })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $rootScope, $http, $firebaseObject, $firebaseArray, $ionicActionSheet, $ionicModal, Items, Auth, $cordovaLocalNotification) {
+.controller('AppCtrl', function($scope, $rootScope, $http, $firebaseObject, $firebaseArray, $ionicActionSheet, $ionicModal, Items, Auth) {
 
   var _self = this;
   _self.users = new Firebase("https://poll2roll.firebaseio.com/users");
@@ -16,24 +16,6 @@ angular.module('starter.controllers', [])
       $scope.modallogin = modallogin;
       $scope.modallogin.show();
   });
-
-  $scope.scheduleDelayedNotification = function () {
-  console.log('wait 10 seconds');
-      var now = new Date().getTime();
-      var _10SecondsFromNow = new Date(now + 10 * 1000);
-
-      $cordovaLocalNotification.schedule({
-        id: 1,
-        title: 'Microsoft Cordova Team',
-        text: 'You will get push notification',
-        badge: 1,
-        at: _10SecondsFromNow
-      }).then(function (result) {
-        // ...
-      });
-    };
-
-    $scope.scheduleDelayedNotification();
 
   //2 separate calls made to Facebook, First call gets the access token and some basic info and second call is 
   //used to get more advanced information. Second call has some limitations at the moment. 
@@ -101,305 +83,240 @@ angular.module('starter.controllers', [])
           username: $scope.authData.displayName,
           userData: userData
         }).then(function(ref) {
-      $scope.key = ref.key();
-      console.log("added record with id " + $scope.key, '/////', $scope.users.$indexFor($scope.key));
-      // returns location in the array
-    });
-
-      }
-    });
-    $scope.index = $scope.users.$indexFor($scope.key);
-  };
-})
-
-.controller('CustomerCtrl', function($scope, $http, $window, $ionicSlideBoxDelegate, $ionicModal, $ionicPlatform) {
-  var _self = this;
-  $scope.X;
-  $scope.Y;
-  $scope.Z;
-  $scope.dynamic = 5;
-  $scope.max = 10;
-  _self.surveySubmitted = false;
-  $scope.prev = 0;
-
-$scope.slideHasChanged = function(index) {
-  if(!$scope.questions[$scope.prev].Rating){
-    $scope.questions[$scope.prev].Rating = $scope.dynamic;
-  }
-  $scope.prev = index;
-  $scope.dynamic = 5;
-}
-
-$scope.closeLogin = function() {
-  $scope.modal.hide();
-};
-
-$scope.rateAgain = function() {
-  $scope.dynamic = $scope.questions[$scope.prev].Rating;
-  $scope.questions[$scope.prev].Rating = null;
-}
-
- $ionicModal.fromTemplateUrl('templates/surveyComplete.html', {
-  scope: $scope
-}).then(function(modal) {
-  $scope.modal = modal;
-});
-
-$scope.submitSurvey = function() {
-  var send={};
-
-  $scope.questions[$scope.prev].Rating = $scope.dynamic;
-
-  for(var i = 0; i < $scope.questions.length; i++) {
-    var val = $scope.questions[i];
-    send[val.$id] = val.Rating || 'NA';
-  }
-
-  $scope.users[$scope.index].feedback = send;
-  console.log('///', $scope.users[$scope.index].feedback);
-
-  $scope.users.$save($scope.index).then(function() {
-  $scope.modal.show();
-  _self.surveySubmitted = true;
-  });
-}
-
-$scope.previous = function() {
-  $ionicSlideBoxDelegate.previous();
-}
-
-$scope.next = function() {
-  $ionicSlideBoxDelegate.next();
-}
-
-function onSuccess(acceleration) {
-    $scope.X = acceleration.x;
-    $scope.Y = acceleration.y;
-    $scope.Z = acceleration.z;
-
-    if($scope.X < -3) {
-      $scope.dynamic += 1;
-    } 
-    // if ($scope.X < -5) {
-    //   $scope.dynamic += 2;
-    // } 
-    // if ($scope.X > 5) {
-    //   $scope.dynamic -= 2;
-    // } 
-    if ($scope.X > 3) {
-      $scope.dynamic -= 1;
-    } 
-
-    if($scope.dynamic > 10) {
-      $scope.dynamic = 10;
-    } else if($scope.dynamic <0 || $scope.dynamic == 0) {
-      $scope.dynamic = 1;
-    }
-    var type;
-
-    if ($scope.dynamic < 4) {
-      type = 'warning';
-    } else {
-      type = 'success';
-    }
-    $scope.detectShake(acceleration); 
-
-    $scope.type = type;
-    $scope.$apply();
-}
-
-function onError() {
-    console.log('accelerometer not working');
-}
-
-var options = { frequency: 500 };  // Update every 3 seconds
-
-var watchID = $window.navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
-
-  
-
-//////////////////////
-
-  // watch Acceleration
-  $scope.options = { 
-    frequency: 100, // Measure every 100ms
-    deviation : 30  // We'll use deviation to determine the shake event, best values in the range between 25 and 30
-  };
-
-  // Current measurements
-  $scope.measurements = {
-    x : null,
-    y : null,
-    z : null,
-    timestamp : null
-  }
-
-  // Previous measurements  
-  $scope.previousMeasurements = {
-    x : null,
-    y : null,
-    z : null,
-    timestamp : null
-  } 
-  
-  // Watcher object
-  $scope.watch = null;
-  
-  // Start measurements when Cordova device is ready
-  $ionicPlatform.ready(function() {
-    console.log('Ionic platform ready');
-    
-    // Detect shake method    
-    $scope.detectShake = function(result) { 
-    
-      //Object to hold measurement difference between current and old data
-      var measurementsChange = {};
-      
-      // Calculate measurement change only if we have two sets of data, current and old
-      if ($scope.previousMeasurements.x !== null) {
-        measurementsChange.x = Math.abs($scope.previousMeasurements.x, result.x);
-        measurementsChange.y = Math.abs($scope.previousMeasurements.y, result.y);
-        measurementsChange.z = Math.abs($scope.previousMeasurements.z, result.z);
-      }
-      
-      // If measurement change is bigger then predefined deviation
-      if (measurementsChange.x + measurementsChange.y + measurementsChange.z > $scope.options.deviation) {
-        //$scope.stopWatching();  // Stop watching because it will start triggering like hell
-        console.log('Shake detected'); // shake detected
-        //setTimeout($scope.startWatching(), 1000);  // Again start watching after 1 sex
-        $scope.submitSurvey();
-        
-        // Clean previous measurements after succesfull shake detection, so we can do it next time
-        $scope.previousMeasurements = { 
-          x: null, 
-          y: null, 
-          z: null
-        }       
-        
-      } else {
-        // On first measurements set it as the previous one
-        $scope.previousMeasurements = {
-          x: result.x,
-          y: result.y,
-          z: result.z
+            $scope.key = ref.key();
+            console.log("added record with id " + $scope.key, '/////', $scope.users.$indexFor($scope.key));
+            // returns location in the array
+            });
         }
-      }         
-    }   
-  });
-
-
-
-  $('.question').fadeIn(400); // Fadeout
-  $('h1,h2').addClass('popone'); // Intro
-  $('.character').addClass('poptwo'); // Intro
-  $('.rating').addClass('popthree'); // Intro
-  $('.next,.prev').addClass('popfour'); // Intro
-
-
-var slide_amount = $('.feedbackform_slide').length; // Slide count
-var window_width = $(window).width(); // Init window width
-var current_x = 0; // Current x value of slides
-var current_position = 0; // Current position
-
-$('.feedbackform').css('width',window_width * slide_amount + 'px'); // Set up the slides
-$('.feedbackform_slide').css('width',window_width + 'px'); // Set up the slides
-
-$(window).resize(function(){ // Responisivity
-  var window_width = $(window).width(); // Window width
-  $('.feedbackform').css('width',window_width * slide_amount + 'px'); // Re jig slide sizes
-  $('.feedbackform_slide').css('width',window_width + 'px'); // Re jig slide sizes
-  current_position = 0; // Reset
-  current_x = current_position * window_width; // Reset
-  $('.feedbackform_slide').css('right',current_x); // Reset
-  $('.active_slide').removeClass('active_slide')
-  $('.first').addClass('active_slide');
-});
-
-// End page slider
-
-// Questions
-
-var questions = ['How satisfied are you with our services?','How satisfied are you with your pet?','How was our staff?','How was the cleanliness of our store?']
-var q = 0;
-
-$('.question_s').each(function(){
-  $(this).html(questions[q])
-  q++;
+      });
+      $scope.index = $scope.users.$indexFor($scope.key);
+  };
 })
 
-// Summary
-var answers = []
+.controller('FeedbackCtrl', function($scope, $http, $window, $ionicSlideBoxDelegate, $ionicModal, $ionicPlatform) {
+    var _self = this;
+    $scope.X;
+    $scope.Y;
+    $scope.Z;
+    $scope.dynamic = 5;
+    $scope.max = 10;
+    _self.surveySubmitted = false;
+    $scope.prev = 0;
 
-var animals = ['monkey','rabbit','panda','lion']
-for(i = 0; i < animals.length; i++){
-  answers[animals[i]] = ['Average'] 
-}
-
-function update_answers(){
-   $('.summary').html('');
-  for(i = 0; i < questions.length; i++){
-    $('.summary').append('<p>' + questions[i] + '</p><p class="answer_p">' + answers[animals[i]][0] + '</p>')
-  }
-}
-// Messages
-
-var monkey_messages = ['terrible','bad','not great','average','good','excellent','amazing']; // Monkey array
-var rabbit_messages = ['dissapointed','unhappy','not great','average','pleased','happy','super happy']; // Rabbit array
-var panda_messages = ['terrible','bad','not great','average','good','excellent','amazing']; // Panda array
-var lion_messages = ['very dirty','dirty','needs work','average','clean','very clean','Superb']; // Lion array
-
-var smile_value;
-
-// $('input').mousedown(function(){
-  var active_smile = 'panda'; // Get active smile
-  var s = setInterval(function(){
-    // Change smile svg coords
-    smile_value = $scope.questions[$scope.prev].Rating*2 || $scope.dynamic*2; // Get the value
-    $('.smile.' + active_smile + ' path').attr('d','M10 10 C 20 ' + smile_value + ', 40 ' + smile_value + ', 50 10');
-    // Select relevant array
-    if(active_smile == 'monkey'){
-      var active_array = monkey_messages; // Monkey array
-    } else if(active_smile == 'rabbit'){
-      var active_array = rabbit_messages; // Rabbit array
-    } else if(active_smile == 'panda'){
-      var active_array = panda_messages; // Panda array
-    } else if(active_smile == 'lion'){
-      var active_array = lion_messages; // Lion array
+    $scope.slideHasChanged = function(index) {
+        if(!$scope.questions[$scope.prev].Rating){
+            $scope.questions[$scope.prev].Rating = $scope.dynamic;
+        }
+        $scope.prev = index;
+        $scope.dynamic = 5;
     }
-    answers[active_smile] = [$('.rating.' + active_smile + ' span').html()]; // Push to answers
-    $('.sb.' + active_smile).css('opacity',(smile_value/60)); // Pattern opacity
-    $('.grad.' + active_smile).css('opacity',(smile_value/40)); // Gradient opacity
-    if(smile_value == 0){
-      // Worst
-      $('.rating.' + active_smile + ' span').html(active_array[0]); // Set message
-    } else if(smile_value < 10 && smile_value > 5){
-      // Bad
-      $('.rating.' + active_smile + ' span').html(active_array[1]); // Set message
-    } else if(smile_value < 5 && smile_value > 0){
-      // Not good
-      $('.rating.' + active_smile + ' span').html(active_array[2]); // Set message
-    } else if(smile_value == 10){
-      // Average
-      $('.rating.' + active_smile + ' span').html(active_array[3]); // Set message
-    } else if(smile_value > 10 && smile_value < 15){
-      // Good
-      $('.rating.' + active_smile + ' span').html(active_array[4]); // Set message
-    } else if(smile_value > 15 && smile_value < 20){
-      // Very good
-      $('.rating.' + active_smile + ' span').html(active_array[5]); // Set message
-    } else if(smile_value== 20){
-      // Amazing
-      $('.rating.' + active_smile + ' span').html(active_array[6]); // Set message
-    }        
-  },100)
-// });
 
-// Clear interval
+    $scope.closeLogin = function() {
+        $scope.modal.hide();
+    };
 
-// $('input').mouseup(function(){
-//   clearInterval(s); // Clear intervals
-// });
+    $scope.rateAgain = function() {
+        $scope.dynamic = $scope.questions[$scope.prev].Rating;
+        $scope.questions[$scope.prev].Rating = null;
+    }
 
+    $ionicModal.fromTemplateUrl('templates/surveyComplete.html', {
+        scope: $scope
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+
+    $scope.submitSurvey = function() {
+        var send={};
+
+        $scope.questions[$scope.prev].Rating = $scope.dynamic;
+
+        for(var i = 0; i < $scope.questions.length; i++) {
+            var val = $scope.questions[i];
+            send[val.$id] = val.Rating || 'NA';
+        }
+
+    $scope.users[$scope.index].feedback = send;
+    console.log('///', $scope.users[$scope.index].feedback);
+
+    $scope.users.$save($scope.index).then(function() {
+        $scope.modal.show();
+        _self.surveySubmitted = true;
+        });
+    }
+
+    $scope.previous = function() {
+        $ionicSlideBoxDelegate.previous();
+    }
+
+    $scope.next = function() {
+        $ionicSlideBoxDelegate.next();
+    }
+
+    function onSuccess(acceleration) {
+        $scope.X = acceleration.x;
+        $scope.Y = acceleration.y;
+        $scope.Z = acceleration.z;
+
+        if($scope.X < -3) {
+            $scope.dynamic += 1;
+        } 
+        
+        if ($scope.X > 3) {
+            $scope.dynamic -= 1;
+        } 
+
+        if($scope.dynamic > 10) {
+            $scope.dynamic = 10;
+        } else if ($scope.dynamic < 0 || $scope.dynamic == 0) {
+            $scope.dynamic = 1;
+        }
+        
+        var type = "info";
+
+        if ($scope.dynamic < 3) {
+            type = 'danger';
+        } else if ($scope.dynamic > 7) {
+            type = 'success';
+        }
+        
+        $scope.detectShake(acceleration); 
+        $scope.type = type;
+        $scope.$apply();
+    }
+
+    function onError() {
+        console.log('accelerometer not working');
+    }
+
+    var options = { frequency: 500 };  // Update every 500 milliseconds
+    $window.navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+
+    // watch Acceleration
+    $scope.options = { 
+        frequency: 100, // Measure every 100ms
+        deviation : 30  // We'll use deviation to determine the shake event, best values in the range between 25 and 30
+    };
+
+    // Current measurements
+    $scope.measurements = {
+        x : null,
+        y : null,
+        z : null,
+        timestamp : null
+    }
+
+    // Previous measurements  
+    $scope.previousMeasurements = {
+        x : null,
+        y : null,
+        z : null,
+        timestamp : null
+    } 
+  
+    // Start measurements when Cordova device is ready
+    $ionicPlatform.ready(function() {
+        console.log('Ionic platform ready');
+
+        // Detect shake method    
+        $scope.detectShake = function(result) { 
+
+            //Object to hold measurement difference between current and old data
+            var measurementsChange = {};
+
+            // Calculate measurement change only if we have two sets of data, current and old
+            if ($scope.previousMeasurements.x !== null) {
+                measurementsChange.x = Math.abs($scope.previousMeasurements.x, result.x);
+                measurementsChange.y = Math.abs($scope.previousMeasurements.y, result.y);
+                measurementsChange.z = Math.abs($scope.previousMeasurements.z, result.z);
+            }
+
+            // If measurement change is bigger then predefined deviation
+            if (measurementsChange.x + measurementsChange.y + measurementsChange.z > $scope.options.deviation) {
+                //$scope.stopWatching();  // Stop watching because it will start triggering like hell
+                console.log('Shake detected'); // shake detected
+                //setTimeout($scope.startWatching(), 1000);  // Again start watching after 1 sex
+                $scope.submitSurvey();
+
+                // Clean previous measurements after succesfull shake detection, so we can do it next time
+                $scope.previousMeasurements = { 
+                    x: null, 
+                    y: null, 
+                    z: null
+                }       
+
+            } else {
+                // On first measurements set it as the previous one
+                $scope.previousMeasurements = {
+                    x: result.x,
+                    y: result.y,
+                    z: result.z
+                }
+            }         
+        }
+    });
+    
+    $('.question').fadeIn(400); // Fadeout
+    $('h1,h2').addClass('popone'); // Intro
+    $('.character').addClass('poptwo'); // Intro
+    $('.rating').addClass('popthree'); // Intro
+    $('.next,.prev').addClass('popfour'); // Intro
+
+
+    var slide_amount = $('.feedbackform_slide').length; // Slide count
+    var window_width = $(window).width(); // Init window width
+    var current_x = 0; // Current x value of slides
+    var current_position = 0; // Current position
+
+    $('.feedbackform').css('width',window_width * slide_amount + 'px'); // Set up the slides
+    $('.feedbackform_slide').css('width',window_width + 'px'); // Set up the slides
+
+    $(window).resize(function(){ // Responisivity
+        var window_width = $(window).width(); // Window width
+        $('.feedbackform').css('width',window_width * slide_amount + 'px'); // Re jig slide sizes
+        $('.feedbackform_slide').css('width',window_width + 'px'); // Re jig slide sizes
+        current_position = 0; // Reset
+        current_x = current_position * window_width; // Reset
+        $('.feedbackform_slide').css('right',current_x); // Reset
+        $('.active_slide').removeClass('active_slide')
+        $('.first').addClass('active_slide');
+    });
+
+    // Messages
+    var active_array = ['terrible','bad','not great','average','good','excellent','amazing']; // Panda array
+    var smile_value;
+
+    var active_smile = 'panda'; // Get active smile
+    setInterval(function() {
+        // Change smile svg coords
+        smile_value = $scope.questions[$scope.prev].Rating*2 || $scope.dynamic*2; // Get the value
+        $('.smile.' + active_smile + ' path').attr('d','M10 10 C 20 ' + smile_value + ', 40 ' + smile_value + ', 50 10');
+
+        $('.sb.' + active_smile).css('opacity',(smile_value/60)); // Pattern opacity
+        $('.grad.' + active_smile).css('opacity',(smile_value/40)); // Gradient opacity
+        if(smile_value == 0){
+            // Worst
+            $('.rating.' + active_smile + ' span').html(active_array[0]); // Set message
+        } else if(smile_value < 10 && smile_value > 5){
+            // Bad
+            $('.rating.' + active_smile + ' span').html(active_array[1]); // Set message
+        } else if(smile_value < 5 && smile_value > 0){
+            // Not good
+            $('.rating.' + active_smile + ' span').html(active_array[2]); // Set message
+        } else if(smile_value == 10){
+            // Average
+            $('.rating.' + active_smile + ' span').html(active_array[3]); // Set message
+        } else if(smile_value > 10 && smile_value < 15){
+            // Good
+            $('.rating.' + active_smile + ' span').html(active_array[4]); // Set message
+        } else if(smile_value > 15 && smile_value < 20){
+            // Very good
+            $('.rating.' + active_smile + ' span').html(active_array[5]); // Set message
+        } else if(smile_value== 20){
+            // Amazing
+            $('.rating.' + active_smile + ' span').html(active_array[6]); // Set message
+        }        
+    }, 100)
 })
 
 
